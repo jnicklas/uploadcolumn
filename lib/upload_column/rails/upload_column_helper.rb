24266ab9fb1fe22f4ebbd62563f700465c99e76e
Jonas Nicklas <jonas.nicklas@gmail.com>
2008-04-09 18:20:10 +0000

    cleaned up the UploadColumnHelper a bit
    git-svn-id: http://uploadcolumn.rubyforge.org/svn/trunk@110 02338ee4-8dee-4235-8d26-5b68cf25713b

--- lib/upload_column/rails/upload_column_helper.rb
+++ lib/upload_column/rails/upload_column_helper.rb
@@ -1,4 +1,4 @@
-module UploadColumnHelper
+module UploadColumn::UploadColumnHelper
 
   # Returns an input tag of the "file" type tailored for accessing an upload_column field
   # (identified by method) on an object assigned to the template (identified by object).
@@ -12,17 +12,16 @@ module UploadColumnHelper
   # Note: if you use file_field instead of upload_column_field, the file will not be
   # stored across form redisplays.
   def upload_column_field(object, method, options={})
-    result = ActionView::Helpers::InstanceTag.new(object, method, self).to_input_field_tag("file", options)
-    result << ActionView::Helpers::InstanceTag.new(object, method.to_s+"_temp", self).to_input_field_tag("hidden", {})
+    file_field(object, method, options) + hidden_field(object, method.to_s + '_temp')
   end
-  
+
   # A helper method for creating a form tag to use with uploadng files,
   # it works exactly like Rails' form_tag, except that :multipart is always true
   def upload_form_tag(url_for_options = {}, options = {}, *parameters_for_url, &proc)
     options[:multipart] = true
     form_tag( url_for_options, options, *parameters_for_url, &proc )
   end
-  
+
   # A helper method for creating a form tag to use with uploadng files,
   # it works exactly like Rails' form_for, except that :multipart is always true
   def upload_form_for(*args, &block)
@@ -30,28 +29,8 @@ module UploadColumnHelper
     options[:html] ||= {}
     options[:html][:multipart] = true
     args.push(options)
-    
+  
     form_for(*args, &block)
-  end
-
-  # Returns an image tag using a URL created by the set of +options+. Accepts the same options
-  # as ActionController::Base#url_for. It's also possible to pass a string instead of an options
-  # hash.
-  #
-  # Example
-  #     image( :action => "solarize_picture", :id => @user )
-  # Use this in conjunction with UploadColumnRenderHelper.render_image to output dynamically
-  # rendered version of your RMagick manipulated images.
-  def image(options = {}, html_options = {})
-    html_options[:src] = if options.is_a?(String) then options else self.url_for(options) end
-    html_options[:alt] ||= File.basename(html_options[:src], '.*').split('.').first.capitalize
-
-    if html_options[:size]
-      html_options[:width], html_options[:height] = html_options[:size].split("x")
-      html_options.delete :size
-    end
-
-    tag("img", html_options)
   end
 
 end
@@ -60,7 +39,7 @@ class ActionView::Helpers::FormBuilder #:nodoc:
   self.field_helpers += ['upload_column_field']  
   def upload_column_field(method, options = {})
     @template.send(:upload_column_field, @object_name, method, options.merge(:object => @object))
-  end 
+  end
 end
 
-ActionView::Base.send(:include, UploadColumnHelper)
+ActionView::Base.send(:include, UploadColumn::UploadColumnHelper)
